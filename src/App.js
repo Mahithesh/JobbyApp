ef3ee69336fcd25e687dbf11a83d887ebbe4d3f2
Mahithesh <mahitheshuja@gmail.com>
2025-10-06 22:13:28 +0530

    Fix redirect to use relative path for GitHub Pages routing

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => (
       <PRoute exact path="/jobs" component={Jobs} />
       <PRoute exact path="/jobs/:id" component={JobDetails} />
       <Route path="/not-found" component={NotFound} />
-      <Redirect to="/not-found" />
+      <Redirect to="not-found" />
     </Switch>
   </div>
 )
